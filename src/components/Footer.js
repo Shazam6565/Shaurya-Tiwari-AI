7e2ccc9e8b4e6a8a7ccf4039de0673fa02367508
Shazam6565 <100082266+Shazam6565@users.noreply.github.com>
2024-02-20 15:58:11 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,13 +17,13 @@ function Footer() {
           <h3>Designed and Developed by Shaurya Tiwari</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} ST</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/Shazam6565"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/elonmusk"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/shauryatiwari/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/sh.a.zam/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
